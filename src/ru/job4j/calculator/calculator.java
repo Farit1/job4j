bd3d27c7a3da2b657fdef5094fc4b9ac6ccc502e
Фарит <farit84@yandex.ru>
2019-10-10 00:33:21 +0500

    works at a job

--- src/ru/job4j/calculator/calculator.java
+++ src/ru/job4j/calculator/calculator.java
@@ -2,7 +2,7 @@ package ru.job4j.calculator;
 
 public class Calculator {
     public static void add(double first, double second) {
-        double result = first+second;
+        double result = first+second+0;
         System.out.println(first + "+"+second+" = "+result);
     }
 
