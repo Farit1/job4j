9eada5cef48b4352e767322655f9de215d87d740
Фарит <farit84@yandex.ru>
2019-10-20 00:08:33 +0500

    4.1. Глупый бот.[#182088]

--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -13,4 +13,15 @@ public class DummyBot {
         }
         return rsl;
     }
+    public static void main(String[] Args){
+        String question1 = "Привет, Бот.";
+        System.out.println("Сообщение боту: "+question1);
+        System.out.println("Бот: "+answer(question1));
+        String question2 = "Пока.";
+        System.out.println("Сообщение боту: "+question2);
+        System.out.println("Бот: "+answer(question2));
+        String question3 = "Как дела?";
+        System.out.println("Сообщение боту: "+question3);
+        System.out.println("Бот: "+answer(question3));
+    }
 }
--- chapter_001/src/main/java/ru/job4j/condition/DummyBotTest.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBotTest.java
@@ -1,22 +1,15 @@
 package ru.job4j.condition;
-/*
+
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-*/
+
 public class DummyBotTest {
-/*    @Test
+   @Test
     public void whereGreenBot() {
         assertThat(
                 DummyBot.answer("Привет, Бот."),
                 is("Привет, умник.")
-        );
-    }
-    @Test
-    public void whenByeBot() {
-        assertThat(
-                DummyBot.answer("Пока."),
-                is("До скорой встречи")
         );
     }
     @Test
@@ -30,7 +23,7 @@ public class DummyBotTest {
     public void whenUnknownBot() {
         assertThat(
                 DummyBot.answer("Сколько будет 2 + 2?"),
-                is("Это ставит меня в тупик. Задайте другой вопрос")
+                is("Это ставит меня в тупик. Задайте другой вопрос.")
         );
-    }*/
+    }
 }
