53f785455f9faf70d9e2c29eedab4e0b7e0fb599
Фарит <farit84@yandex.ru>
2019-10-19 22:30:48 +0500

    4.1. Глупый бот.[#182088]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            // Заменить ... на правильный ответ rsl = "ответ по заданию".
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        } else {
+            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        }
+        return rsl;
+    }
+}
