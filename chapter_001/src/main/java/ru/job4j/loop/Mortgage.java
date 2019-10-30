a76c78774c8084665c86ac227aac7a705a56b0b6
Farit1 <farit84@yandex.ru>
2019-10-31 00:32:47 +0500

    5.7. Ипотека.[#182106]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,10 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    //amount - сумма выданная по кредиту, salary - годовой доход, percent - процентная ставка по кредиту.
+
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        return year;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/loop/MortgageTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MortgageTest {
+    @Test
+    public void wen1Year() {
+        Mortgage montgage = new Mortgage();
+        int year = montgage.year(1000, 1200, 1);
+        assertThat(year, is(1));
+    }
+    public void wen2Year() {
+        Mortgage montgage = new Mortgage();
+        int year = montgage.year(100, 120, 50);
+        assertThat(year, is(2));
+    }
+}
