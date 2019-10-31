a0a892116b6a663b0c128f21bbf6cf5a25253154
Farit1 <farit84@yandex.ru>
2019-10-31 19:09:35 +0500

    5.6. Простые числа.[#182105]

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -2,18 +2,22 @@ package ru.job4j.loop;
 
 public class PrimeNumber {
     public int  calc(int finish) {
+        CheckPrimeNumber cpn = new CheckPrimeNumber();
         int count = 0;
         boolean prime;
         // Внешний цикл используется для прохода по всем числам от 2 до finish.
         for (int i = 2; i <= finish; i++) {
+            /* Внутренний цикл определения простого числа
             prime = true;
-            //Внутренний цикл используется для проверки деления без остатка.
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
                     prime = false;
                     break;
                 }
             }
+            */
+            // Проверка простого числа посредством обращения к классу CheckPrimeNumber
+            prime = cpn.check(i);
             if (prime == true) {
                 count++;
             }
