5571b9c5ddf94aa7d41754da7855e1c6fdbe0a18
Farit1 <farit84@yandex.ru>
2019-11-11 00:31:23 +0500

    5.8. JavaDoc. Code convention[#182102]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate
+ * @author Farit Shaikhytdinov (farit84@yandex.ru)
+ */
+public class Calculate {
+
+    /**
+     * Method echo.
+     * @param name - Your name
+     * @return Echo plus your name
+     */
+    public String echo(String name) {
+        return name;
+    }
+
+    /**
+     * Конструктор, вывод строки в консоль
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ * @author Farit Shaikhytdinov (farit84@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ *
+ */
+package ru.job4j.calculate;
