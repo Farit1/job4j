4df0b1c835e00692bb2237a02dbfb12f39bf54d5
Farit1 <farit84@yandex.ru>
2019-11-14 01:09:25 +0500

    6.0. Заполнить массив степенями чисел.[#182110]

--- chapter_001/src/main/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/main/java/ru/job4j/array/SquareTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertThat;
 
 public class SquareTest {
     /**
-     * Method whenBound3Then149 - Метод выполняет тестирование метода Square.
+     * Method whenBound3Then149 - Метод выполняет тестирование метода Square с массивом {1,4,9}.
      */
     @Test
     public void whenBound3Then149() {
@@ -18,6 +18,30 @@ public class SquareTest {
         Square square = new Square();
         int[] rst = square.calculate(bound);
         int[] except = new int[] {1,4,9};
+        assertThat(rst, is(except));
+    }
+
+    /**
+     * Method whenBound3Then14916 - Метод выполняет тестирование метода Square с массивом {1,4,9,16}.
+     */
+    @Test
+    public void whenBound3Then14916() {
+        int bound = 4;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] except = new int[] {1,4,9,16};
+        assertThat(rst, is(except));
+    }
+
+    /**
+     * Method whenBound3Then1491625 - Метод выполняет тестирование метода Square с массивом {1,4,9,16,25}.
+     */
+    @Test
+    public void whenBound3Then1491625() {
+        int bound = 5;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] except = new int[] {1,4,9,16,25};
         assertThat(rst, is(except));
     }
 }
