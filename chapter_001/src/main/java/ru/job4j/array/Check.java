5548d507d156856cf7615b8286880ca1a4227524
Farit1 <farit84@yandex.ru>
2019-11-17 03:01:03 +0500

    6.3. Массив заполнен true или false[#182114]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -12,9 +12,10 @@ public class Check {
     public boolean mono(boolean[] data) {
         boolean result = false;
         for (int i = 1; i <= data.length-1; i++) {
-            if (data[0]=data[i]) {
+            if (data[0]==data[i]) {
                 result = true;
             } else {
+                result = false;
                 break;
             }
         }
--- chapter_001/src/main/java/ru/job4j/array/CheckTest.java
+++ chapter_001/src/main/java/ru/job4j/array/CheckTest.java
@@ -10,7 +10,10 @@ import static org.junit.Assert.assertThat;
 public class CheckTest {
     @Test
     /**
-     * Method whenDataMonoByTrueThenTrue - выполняет тестирование метода check, который выполняет проверку массива, где все элементы = true
+     * Method whenDataMonoByTrueThenTrue -
+     * выполняет тестирование метода check,
+     * который выполняет проверку массива,
+     * где все элементы = true.
      */
     public void whenDataMonoByTrueThenTrue() {
         Check check = new Check();
@@ -19,7 +22,10 @@ public class CheckTest {
         assertThat(result, is(true));
     }
     /**
-     * Method whenDataNotMonoByTrueThenFalse - выполняет тестирование метода check, который выполняет проверку массива, где один элемент оличается от остальных
+     * Method whenDataNotMonoByTrueThenFalse -
+     * выполняет тестирование метода check,
+     * который выполняет проверку массива,
+     * где один элемент оличается от остальных.
      */
     @Test
     public void whenDataNotMonoByTrueThenFalse() {
@@ -27,5 +33,44 @@ public class CheckTest {
         boolean[] input = new boolean[] {true, false, true};
         boolean result = check.mono(input);
         assertThat(result, is(false));
+    }
+    @Test
+    /**
+     * Method whenArrayDataEvenElementsMonoByFalseThenTrue -
+     * выполняет тестирование метода check,
+     * который выполняет проверку массива с четным количеством элементов,
+     * где все элементы = false.
+     */
+    public void whenArrayDataEvenElementsMonoByFalseThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, false, false, false, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+    @Test
+    /**
+     * Method whenArrayDataEvenElementsMonoByTrueThenTrue -
+     * выполняет тестирование метода check,
+     * который выполняет проверку массива c четным количеством элементов,
+     * где один элементы = false,  остальные true.
+     */
+    public void whenArrayDataEvenElementsNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true, true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+    @Test
+    /**
+     * Method whenArrayDataOddElementsMonoByFalseThenTrue -
+     * выполняет тестирование метода check, который выполняет проверку массива,
+     * c нечетным количеством элементов,
+     * где все элементы = false.
+     */
+    public void whenArrayDataOddElementsMonoByFalseThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {false, false, false, false, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
     }
 }
