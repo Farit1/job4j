61d24c8b10946e0b2a379e78f279b87441dd3a4f
Farit1 <farit84@yandex.ru>
2019-11-18 23:48:47 +0500

    6.5. Поиск индекса в диапазоне.[#182122]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -8,11 +8,13 @@ public class FindLoop {
      * Method indexOf - Метод выполняет поиск элемента в массиве.
      * @param data - массив
      * @param el - искомое значение в массиве
+     * @param start - индекс с которого начинаем поиск
+     * @param finish - индекс которым заканчиваем поиск
      * @return rst - Возвращает индекс поиска
      */
-    public int indexOf(int[] data, int el) {
+    public int indexOf(int[] data, int el, int start, int finish) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
-        for (int index = 0; index <= data.length; index++) {
+        for (int index = start; index <= finish; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
--- chapter_001/src/main/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoopTest.java
@@ -11,13 +11,44 @@ public class FindLoopTest {
     /**
      * Method whenArrayHas5Then0 - Метод выполняет тестирование метода FindLoop с массивом {5,10,13}.
      */
-    @Test
+/*    @Test
     public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
         int[] input = new int[] {5, 10, 3};
         int value = 5;
         int result = find.indexOf(input, value);
         int expect = 0;
+        assertThat(result, is(expect));
+    }*/
+    /**
+     * Method whenFind3 - Метод выполняет тестирование метода FindLoop с массивом {5, 2, 10, 2, 4}.
+     * начиная с индекса 2 по 4
+     */
+    @Test
+    public void whenFind3() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = find.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Method whenFind4 - Метод выполняет тестирование метода FindLoop с массивом {5, 2, 10, 2, 4}.
+     * начиная с индекса 2 по 4
+     */
+    @Test
+    public void whenFind4() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 6;
+        int start = 2;
+        int finish = 4;
+        int result = find.indexOf(input, value, start, finish);
+        int expect = -1;
         assertThat(result, is(expect));
     }
 }
